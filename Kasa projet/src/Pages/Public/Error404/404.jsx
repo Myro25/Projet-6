84a3079244ac320d1b5c7e11032a265be4b8324b
Myro25 <132125266+Myro25@users.noreply.github.com>
2024-03-13 18:15:00 +0100

    page 404

--- Kasa projet/src/Pages/Public/Error404/404.jsx
+++ Kasa projet/src/Pages/Public/Error404/404.jsx
@@ -1,11 +1,13 @@
 // 404.jsx
+import '../../../assets/styles/scss/pages/_404.scss'
 import React from 'react';
-
+import { NavLink } from 'react-router-dom';
 const Error404 = () => {
     return (
-        <div>
-            <h1>404 - Page Not Found</h1>
-            {/* Ajoutez du contenu supplémentaire ici si nécessaire */}
+        <div className='error'>
+            <h1 className='error__title'>404</h1>
+            <p className='error__paragraphe'>Oups! La page que vous demandez n'existe pas.</p>
+            <NavLink to="/home" className='error__button'>Retourner sur la page d’accueil</NavLink>
         </div>
     );
 };
