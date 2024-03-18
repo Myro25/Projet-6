c4ddfc596cccd44577652a2409cc9bf8579694a2
Myro25 <132125266+Myro25@users.noreply.github.com>
2024-03-18 23:09:54 +0100

    amelioration du Sass

--- Kasa projet/src/Pages/Public/Logement/fiche-logement.jsx
+++ Kasa projet/src/Pages/Public/Logement/fiche-logement.jsx
@@ -62,11 +62,21 @@ const FicheLogement = () => {
                     <img src={logement.picture} alt={logement.name} />
                 </div>
                 <div className="details_container_details">
-                    <button onClick={toggleDescription}>Afficher la description</button>
+                    <button onClick={toggleDescription}>
+                        Description
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                            <path d="M14.7897 10.7897C15.4591 10.1202 16.5462 10.1202 17.2157 10.7897L27.4979 21.0719C28.1674 21.7414 28.1674 22.8285 27.4979 23.4979C26.8285 24.1673 25.7414 24.1673 25.072 23.4979L16 14.4259L6.92804 23.4926C6.25862 24.162 5.17148 24.162 4.50206 23.4926C3.83265 22.8231 3.83265 21.736 4.50206 21.0666L14.7843 10.7843L14.7897 10.7897Z" fill="white" />
+                        </svg>
+                    </button>
                     {showDescription && (
                         <p className="details_equipments_container">{logement.description}</p>
                     )}
-                    <button onClick={toggleEquipments}>Afficher les équipements</button>
+                    <button onClick={toggleEquipments}>
+                        Équipements
+                        <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                            <path d="M14.7897 10.7897C15.4591 10.1202 16.5462 10.1202 17.2157 10.7897L27.4979 21.0719C28.1674 21.7414 28.1674 22.8285 27.4979 23.4979C26.8285 24.1673 25.7414 24.1673 25.072 23.4979L16 14.4259L6.92804 23.4926C6.25862 24.162 5.17148 24.162 4.50206 23.4926C3.83265 22.8231 3.83265 21.736 4.50206 21.0666L14.7843 10.7843L14.7897 10.7897Z" fill="white" />
+                        </svg>
+                    </button>
                     {showEquipments && (
                         <div className="details_description_container">
                             {logement.equipments.map((equipment, index) => (
